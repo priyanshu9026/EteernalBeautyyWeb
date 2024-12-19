560755c676dd2d338a525e10cf154443563284a3
priyanshu9026 <singpriyasnshu22@gmail.com>
2024-12-19 08:19:23 +0530

    Change over time

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import Header from './components/Header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/HomePage/Home';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
+     <Home />
     </div>
   );
 }
